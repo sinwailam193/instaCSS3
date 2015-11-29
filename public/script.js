d023ce17488cd4d61d9d6c0cbb0ffaac4e487335
Sin Wai Lam <sinwailam193@hotmail.com>
2015-11-29 10:41:09 -0800

    get picture from instagram before the page loads

--- public/script.js
+++ public/script.js
@@ -1,22 +1,22 @@
+  
+$.ajax({
+  url: '/images',
+  type: "GET",
+  contentType: 'application/json; charset=utf-8',
+  success: function(data){
+    var start = "";
+    for(var i = 0; i < 15; i++){
+      start += "<a target='_blank' href='" + data[i].link + "'><img height='150' width='150' src='" + data[i].images.standard_resolution.url + "' class='boingInUp' /></a>";
+    }
+    $('#instafeed').html(start);
+  },
+  error: function(){
+    console.log('there is an error!');
+  },
+  timeout: 120000
+});
+
 $(function(){
-
-  $.ajax({
-    url: '/images',
-    type: "GET",
-    contentType: 'application/json; charset=utf-8',
-    success: function(data){
-      var start = "";
-      for(var i = 0; i < 15; i++){
-        start += "<a href='" + data[i].link + "'><img height='150' width='150' src='" + data[i].images.standard_resolution.url + "' class='boingInUp' /></a>";
-      }
-      $('#instafeed').html(start);
-    },
-    error: function(){
-      console.log('there is an error!');
-    },
-    timeout: 120000
-  });
-
   $('#magic').on('click', function(){
     $('#instafeed img').removeClass($('#instafeed img').attr('class'));
     $('#instafeed img').toggleClass("shake")
